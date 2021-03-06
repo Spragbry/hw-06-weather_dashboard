91523ae27afe55b396bd6539858f5d4b1d76a912
Bryce Spraggins <spragbry93@gmail.com>
2021-03-05 19:12:32 -0500

    JS parameters and CSS styling

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const FIVEDAY_WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 const TODAYS_WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
+const UVI_ENDPOINT = "http://api.openweathermap.org/data/2.5/uvi"
 const API_KEY = "0316fa16dc8334829cbab528b156ea77"
 let query = ""
+
+const fetchUVIndex = async(lat, lon) => {
+    const url = `${UVI_ENDPOINT}?lat=${lat}&appid=${API_KEY}&units=imperial&lon=${lon}`
+    const response = await fetch(url, {method:"GET"})
+    const json = await response.json()
+    return json 
+}
 
 const fetchFiveDayForcast = async (city) => {
     const url = `${FIVEDAY_WEATHER_ENDPOINT}?q=${city}&appid=${API_KEY}&units=imperial`
     const response = await fetch(url, {method:"GET"})
     const json = await response.json()
+    console.log("FetchFiveDayForcast", json)
     let result = {
         five_day_forcast:[],
         city_name: json.city.name
@@ -21,8 +30,8 @@ const fetchFiveDayForcast = async (city) => {
         const date = moment(dateStr)
         if (today.clone().add(offset, "days").dayOfYear() == date.dayOfYear()){
             if (date.hour() == hour){
-                const weather = item.main
-                console.log("weather",weather)
+                let weather = item.main
+                weather.windSpeed = item.wind.speed
                 const result = {
                     date,
                     weather 
@@ -37,31 +46,67 @@ const fetchFiveDayForcast = async (city) => {
 }
 
 
-fetchTodaysForcast = async (city) => {
+const fetchTodaysForcast = async (city) => {
     const url = `${TODAYS_WEATHER_ENDPOINT}?q=${city}&appid=${API_KEY}&units=imperial`
     const response = await fetch(url, {method:"GET"})
     const json = await response.json()
+    console.log("today json", json)
+    const lat = json.coord.lat
+    const lon = json.coord.lon
+    const uvIndex = await fetchUVIndex(lat, lon)
+    console.log("UV Index", uvIndex)
     let result = {
         date: moment(),
         weather: json.main,
         city: json.name
     }
+    result.weather.windSpeed = json.wind.speed
+    result.weather.UV = uvIndex.value
     return result 
 }
 
+const setTodaysUI = (data) => {
+    const weather = data.weather
+    const dateStr = `Date: ${data.date.format("M-D-YYYY")}`
+    const temp = `Temperature: ${weather.temp}`
+    const name = `Name: ${data.city}`
+    const windSpeed = `Wind Speed: ${weather.windSpeed}`
+    const UV = `UV Index: ${weather.UV}`
+    const uvClasses = ["favorable","moderate","severe"]
+    $("#ci-name").html(name)
+    $("#ci-date").html(dateStr)
+    $("#ci-temp").html(temp)
+    $("#ci-windSpeed").html(windSpeed)
+    $("#ci-uvi").html(UV)
+}
+
+const setFiveDayUI = (data) => {
+    console.log("five day container", $("#fiveDay"))
+    console.log("five day data", data)
+    data.five_day_forcast.forEach((day) => {
+        const dateStr = day.date.format("M-D-YYYY")
+        const temp = day.weather.temp
+        const humidity = day.weather.humidity
+        $("#five-day").append(`<div id="five-day-element">
+            <h5 class="five-day-text">${dateStr}</h5>
+            <h5 class="five-day-text">Temperature: ${temp}&#8457;</h5>
+            <h5 class="five-day-text">Humidity: ${humidity}&#x0025;</h5>
+        </div>`)
+    })
+    console.log("five day container added", $("#fiveDay"))
+}
+
+
 $(document).ready(() => {
-    console.log("doc loaded")
     $("#query-input").keyup((event) => {
         if (event.target && event.target.value){
             query = event.target.value
         }
-        console.log("text input change", event)
     })
     $("#search-btn").click(async () => {
         const todaysForcast = await fetchTodaysForcast(query)
         const fiveDayForcast = await fetchFiveDayForcast(query)
-        console.log("Todays Forcast", todaysForcast)
-        console.log("five day forcast", fiveDayForcast)
+        setTodaysUI(todaysForcast)
+        setFiveDayUI(fiveDayForcast)
     })
-    console.log("query input", $("#query-input"))
 })
